83e53971ad59efe78fc2d74be4cf532a5f3c83ed
khosek <khosek@iu.edu>
2022-12-04 15:36:45 -0500

    Fixes dialogue start bug

--- NPC/NPC.gd
+++ NPC/NPC.gd
@@ -14,11 +14,12 @@ func _ready():
 	Dialogue.connect("finished_dialogue", self, "finished")
 
 
-func _on_Area_body_entered(body):
+func _on_Area_body_entered(_body):
 	Dialogue.start_dialogue(dialogue)
+#	print("area entered")
 
 
-func _on_Area_body_exited(body):
+func _on_Area_body_exited(_body):
 	Dialogue.hide_dialogue()
 
 func finished():
